4146f09629f6478d3a9ae3570efa94933f54b83f
Shesh <sheshkash@gmail.com>
2025-05-12 19:20:06 +0530

    Deployment issue fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = 'sheshkash.github.io'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}`
 })
